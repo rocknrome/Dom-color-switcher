da2d00ad2563b901dbc6ee4d04dd12dd7eda9f75
Rock N Rome <126816805+rocknrome@users.noreply.github.com>
2023-06-24 02:32:58 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-// Write your DOM code here!
+const btnRed = document.getElementById('btn-red');
+const btnWhite = document.getElementById('btn-white');
+const btnBlue = document.getElementById('btn-blue');
+const btnYellow = document.getElementById('btn-yellow');
+
+btnRed.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'red';
+});
+
+btnWhite.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'white';
+});
+
+btnBlue.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'blue';
+});
+
+btnYellow.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'yellow';
+});
